4a8c1b454715a66c39f304f90a5133ec934160f9
Wendelcorp <bryce@brycewendelaar.com>
2017-08-06 10:33:12 -0400

    CSS updates

--- src/components/Domain.js
+++ src/components/Domain.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
 
 class Domain extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      visibility: "hidden"
+    };
+  }
+  onClick() {
+      this.setState({ visibility: "visible" });
+      setTimeout(() => {
+        this.setState({ visibility: "hidden" })
+      }, 700)
+  }
+
   render() {
+
     return (
-      <div className="domain">
+      <div className="domain" onClick={this.onClick.bind(this)}>
         <h3 data-clipboard-text={this.props.url} className="short-url">{this.props.name + "/" + this.props.ext}</h3>
+        <p id="copied" style={{visibility: this.state.visibility}}>Copied!</p>
       </div>
     );
   }
